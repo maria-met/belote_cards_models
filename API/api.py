4097b58f7a4d5c26e95c734a7faea64ce169969f
mariame tarsi <mariame.tarsi@gmail.com>
2024-03-05 13:53:31 +0100

    initial commit

---
+++ API/api.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from ultralytics import YOLO
+import numpy as np
+
+app = FastAPI()
+
+# Allowing all middleware is optional, but good practice for dev purposes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+img = '../image/WIN_20240304_11_20_31_Pro.jpg'
+
+# http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
+@app.get("/predict")
+def predict(img):      # 1
+    """
+    make prediction based on the image captured
+    """
+
+    model = YOLO('../notebooks/runs/classify/train2/weights/best.pt')  # load a custom model
+    predicted_label = model(img)
+
+
+
+
+@app.get("/")
+def root():
+    return {'greeting': 'Hello'}
