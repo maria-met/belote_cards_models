492006beedad364ba4f6e8f217276aa2daf96e99
mariame tarsi <mariame.tarsi@gmail.com>
2024-03-05 18:06:31 +0100

    add api part

--- API/api.py
+++ API/api.py
@@ -1,8 +1,12 @@
-import pandas as pd
 from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from typing import Tuple # A library for type hints.
+from io import BytesIO # A class for working with binary data in memory.
+from PIL import Image # A library for image processing.
 from fastapi.middleware.cors import CORSMiddleware
 from ultralytics import YOLO
 import numpy as np
+from starlette.responses import Response
 
 app = FastAPI()
 
@@ -15,19 +19,66 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-img = '../image/WIN_20240304_11_20_31_Pro.jpg'
+images = [
+       '/home/mariame/code/maria-met/belote_cards_models/image/WIN_20240304_11_19_18_Pro.jpg',
+       '/home/mariame/code/maria-met/belote_cards_models/image/WIN_20240304_11_20_05_Pro.jpg',
+       '/home/mariame/code/maria-met/belote_cards_models/image/WIN_20240304_11_20_31_Pro.jpg',
+       '/home/mariame/code/maria-met/belote_cards_models/image/WIN_20240304_15_04_52_Pro.jpg',
+       '/home/mariame/code/maria-met/belote_cards_models/image/WIN_20240304_15_05_51_Pro.jpg',
+       '/home/mariame/code/maria-met/belote_cards_models/image/WIN_20240304_15_07_28_Pro.jpg',
+       '/home/mariame/code/maria-met/belote_cards_models/image/WIN_20240304_15_07_56_Pro.jpg'
+       ]
+model = YOLO('/home/mariame/code/maria-met/belote_cards_models/notebooks/runs/classify/train2/weights/best.pt')  # load a custo
 
-# http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
-@app.get("/predict")
-def predict(img):      # 1
+def read_file_as_image(data) -> Tuple[np.ndarray, Tuple[int, int]]: # A function to read the image file as a numpy array
+    img = Image.open(BytesIO(data)).convert('RGB') # Open the image and convert it to RGB color space
+    img_resized = img.resize((180, 180), resample=Image.BICUBIC) # Resize the image to 180 x 180
+    image = np.array(img_resized) # Convert the image to a numpy array
+    return image, img_resized.size # Return the image and its size
+
+# http://127.0.0.1:8000/predict
+@app.post("/predict")
+async def predict(img: UploadFile=File(...)):      # 1
     """
     make prediction based on the image captured
     """
+    # image = await img.read() # Read the image file
+    # nparr = np.fromstring(image, np.uint8)
 
-    model = YOLO('../notebooks/runs/classify/train2/weights/best.pt')  # load a custom model
-    predicted_label = model(img)
+    image, img_size = read_file_as_image(await img.read()) # Read the image file
+    img_batch = np.expand_dims(image, 0) # Add an extra dimension to the image so that it matches the input shape of the model
+    results = model(img_batch)
+    names_dict = results[0].names
 
+    probs = results[0].probs.data.tolist()
 
+    #print(names_dict)
+    #print(probs)
+    label = names_dict[np.argmax(probs)]
+    confidence = np.argmax(probs)
+    print(label)
+    return { # Return the prediction
+            'class': label,
+            'confidence': float(confidence)
+        }
+
+    # labels = []
+    # for img in images:
+    #     results = model(img)
+    #     names_dict = results[0].names
+
+    #     probs = results[0].probs.data.tolist()
+
+    #     #print(names_dict)
+    #     #print(probs)
+    #     label = names_dict[np.argmax(probs)]
+    #     confidence = np.argmax(probs)
+    #     print(label)
+    #     labels.append({ # Return the prediction
+    #             'class': label,
+    #             'confidence': float(confidence)
+    #         })
+    # return labels
 
 
 @app.get("/")
